0d45d9a601ab50897957afeadd9bef76e0ff21fe
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-01-16 23:07:58 +0800

    - var

---
+++ learn-typescript-full-tutorial/variable.ts
@@ -0,0 +1,14 @@
+let greetings: string = "kkkk";
+
+/*
+wouldnt work
+
+greetings = 6
+greetings = true
+*/
+const lowered = greetings.toLowerCase();
+
+console.log(greetings);
+console.log(lowered);
+// removes "Cannot redeclare block-scoped variable 'greetings'
+export {};
