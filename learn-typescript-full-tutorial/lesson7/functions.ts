d1ccd2b6536365ae42888199b72888de49dd75cf
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-01-17 11:09:26 +0800

    - functions

--- learn-typescript-full-tutorial/lesson7/functions.ts
+++ learn-typescript-full-tutorial/lesson7/functions.ts
@@ -25,4 +25,50 @@ function getUpperWithString(val: string) {
 // arrow function with default value for age
 let fx1 = (name: string, age: number = 10) => {};
 
+function returnString(): string {
+  return "hello";
+}
+
+// function thata ccepts another function as argument
+// need to have similar signature just like delegates in c#
+function processor(
+  a: number,
+  b: number,
+  operation: (x: number, y: number) => number
+): number {
+  return operation(a, b);
+}
+
+function add(x: number, y: number) {
+  return x + y;
+}
+
+processor(1, 5, add);
+
+//multitype return????
+// union type
+function returnAnything(a: number, b: number): boolean | string {
+  if (a === b) return true;
+  return "not similar";
+}
+
+// arrow function
+const hello = (s: string): string => {
+  return "";
+};
+
+//arrow fx tht accepts string and return string
+const heroes = ["plastincman", "batman"];
+heroes.map((hero: string): string => {
+  return `hero is ${hero}`;
+});
+
+// never: terminates the program i.e throwing error
+// void: holds data (undefined or null)
+function handleError(errorMessage: string): never {
+  throw new Error(errorMessage);
+  // type string not assignable to never
+  //return "cock"
+}
+
 export {};
