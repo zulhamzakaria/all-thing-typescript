efc72008c2c1e080c2f2bb219438ff4885373d49
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-01-18 13:10:18 +0800

    - modifiers

--- learn-typescript-full-tutorial/projectsetup/dist/index.js
+++ learn-typescript-full-tutorial/projectsetup/dist/index.js
@@ -1,8 +1,26 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 class User {
+    constructor(name, age) {
+        // readonly effective outside of constructor
+        this.ssn = "JN908872-43KL";
+        this.name = name;
+        this.age = age;
+        this.ssn = "000";
+    }
+}
+class User2 {
     constructor(name, age) {
         this.name = name;
         this.age = age;
+        this.name = name;
+        this.age = age;
+    }
+}
+class UserFromType {
+    constructor(user) {
+        user.name = "jonny";
+        user.age = 78;
     }
 }
 const client = new User("jonny", 18);
--- learn-typescript-full-tutorial/projectsetup/src/index.ts
+++ learn-typescript-full-tutorial/projectsetup/src/index.ts
@@ -1,10 +1,32 @@
 class User {
   name: string;
   age: number;
+  // readonly effective outside of constructor
+  private readonly ssn: string = "JN908872-43KL";
   constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+    this.ssn = "000";
+  }
+}
+
+class User2 {
+  constructor(public name: string, public age: number) {
     this.name = name;
     this.age = age;
   }
 }
+
+class UserFromType {
+  constructor(user: UserType) {
+    user.name = "jonny";
+    user.age = 78;
+  }
+}
+
+export type UserType = {
+  name: string;
+  age: number;
+};
 
 const client = new User("jonny", 18);
