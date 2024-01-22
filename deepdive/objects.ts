88224ffda51b697cac2ae26811ec9766bd6f65d7
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-01-22 20:38:48 +0800

    - object

---
+++ deepdive/objects.ts
@@ -0,0 +1,12 @@
+const vr1 = { number1: 1, number2: 2, number3: 3, number4: 4 };
+const var2: { number1: number; number2: number } = {
+  number1: 1,
+  number2: 2,
+};
+
+function printUser(): { name: string; age: number } {
+  return {
+    name: "conn",
+    age: 18,
+  };
+}
