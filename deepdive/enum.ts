19934f9283b73f167896a2492bb11dee4e80baba
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-01-23 15:00:41 +0800

    - enum

---
+++ deepdive/enum.ts
@@ -0,0 +1,10 @@
+enum AccountType {
+  savings,
+  current = "Current",
+}
+
+// enum always shows the value
+// 0
+console.log(`Account type is ${AccountType.savings} 😃`);
+// current
+console.log(`Account type is ${AccountType.current}`);
