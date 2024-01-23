00dba03f00ef57e144b49e7921d314bf732ea404
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-01-23 12:13:16 +0800

    - tuples

---
+++ deepdive/tuples.ts
@@ -0,0 +1,8 @@
+const array: Array<string> = ["1"];
+
+const myTuple: [string, string] = ["hellow", "worrld"];
+console.log(myTuple[1]);
+
+// destructuring
+const [string1, string2] = myTuple;
+console.log(string1);
