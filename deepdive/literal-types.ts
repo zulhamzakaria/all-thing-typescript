6d2fa82d559d31751c137a9a392a77590b15cb7a
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-01-23 12:06:49 +0800

    - literal types

---
+++ deepdive/literal-types.ts
@@ -0,0 +1,17 @@
+let color: "red" | "blue" | "green";
+color = "red";
+color = "green";
+//wrong
+// color = "yellow"
+
+let agree: true;
+agree = true;
+//wrong
+// agree= false;
+
+let num: 1 | 2;
+num = 1;
+//wrong
+// num = 3;
+
+
