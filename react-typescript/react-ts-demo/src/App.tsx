c2767229fe52ba9f263b15374305fe53fa034263
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-10 15:29:51 +0800

    - input - handleChange event

--- react-typescript/react-ts-demo/src/App.tsx
+++ react-typescript/react-ts-demo/src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Button } from "./components/Button";
 import Greet from "./components/Greet";
 import { Heading } from "./components/Heading";
+import { Input } from "./components/Input";
 import { Person } from "./components/Person";
 import { PersonList } from "./components/PersonList";
 import { Status } from "./components/Status";
@@ -35,6 +36,7 @@ function App() {
           alert(e.currentTarget);
         }}
       />
+      <Input value="" handleChange={(e) => alert(e.type)} />
     </div>
   );
 }
---
+++ react-typescript/react-ts-demo/src/components/Input.tsx
@@ -0,0 +1,8 @@
+type InputProps = {
+  value: string;
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export const Input = ({ value, handleChange }: InputProps) => {
+  return <input type="text" value={value} onChange={handleChange} />;
+};
