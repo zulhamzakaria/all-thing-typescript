ea7756e31738702d30902f888fc6184ccbe88abf
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-05-10 12:31:23 +0800

    -type list  - const is an object  - define the object as an array inside the type itself not as an arg

--- react-typescript/react-ts-demo/src/App.tsx
+++ react-typescript/react-ts-demo/src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Greet from "./components/Greet";
 import { Person } from "./components/Person";
+import { PersonList } from "./components/PersonList";
 
 function App() {
   const PersonName = {
@@ -8,10 +9,17 @@ function App() {
     last: "Wayne",
   };
 
+  const names = [
+    { first: "A", last: "B" },
+    { first: "C", last: "D" },
+    { first: "E", last: "F" },
+  ];
+
   return (
     <div className="App">
       <Greet name={"joni"} messageCount={10} isLoggedId={false} />
       <Person name={PersonName} />
+      <PersonList names={names} />
     </div>
   );
 }
