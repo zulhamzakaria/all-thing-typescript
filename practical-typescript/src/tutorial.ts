0c75fc8607895c054d00988d50603b4bd82e9a66
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-07-06 21:07:17 +0800

    - literal types

--- practical-typescript/src/tutorial.ts
+++ practical-typescript/src/tutorial.ts
@@ -1,15 +1,15 @@
-console.log('TS')
+console.log("TS");
 
-type Sex = 'male'|'female'|'other'
+type Sex = "male" | "female" | "other";
 
-const name: string = 'putih'
+const name: string = "putih";
 name.toLocaleUpperCase();
-const age:number = 1
-const sex:Sex = 'female'
+const age: number = 1;
+const sex: Sex = "female";
 
-let postcode: number|string
-postcode = 90990
-postcode = 'malibu-99'
+let postcode: number | string;
+postcode = 90990;
+postcode = "malibu-99";
 
-let response : 'success'|'pending'|'error'
-response='error'
+let response: "success" | "pending" | "error";
+response = "error";
