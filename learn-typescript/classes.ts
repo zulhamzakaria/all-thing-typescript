b5a59a882fcbd3c81a6f3593617cb3a04d9a4fcf
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-19 19:52:26 +0800

    - basic class

---
+++ learn-typescript/classes.ts
@@ -0,0 +1,16 @@
+class User {
+  localFirstName: string;
+  localLastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.localFirstName = firstName;
+    this.localLastName = lastName;
+  }
+
+  getFullName(): string {
+    return this.localFirstName + this.localLastName;
+  }
+}
+
+const user = new User("jonny", "boy");
+console.log(user.getFullName());
