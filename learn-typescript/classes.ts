c1939068e6bb69132e8fe5076a453075ce74ac55
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-19 19:55:51 +0800

    - implement interface

--- learn-typescript/classes.ts
+++ learn-typescript/classes.ts
@@ -1,10 +1,14 @@
-class User {
+class User implements UserInterface {
   private localFirstName: string;
   private localLastName: string;
 
   constructor(firstName: string, lastName: string) {
     this.localFirstName = firstName;
     this.localLastName = lastName;
+  }
+
+  displayAge(): number {
+    return 20;
   }
 
   getFullName(): string {
@@ -14,3 +18,7 @@ class User {
 
 const user = new User("jonny", "boy");
 console.log(user.getFullName());
+
+interface UserInterface {
+  displayAge(): number;
+}
