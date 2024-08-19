476f98aba3e604a2d852b148ea325b7f235ca5eb
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-19 20:16:39 +0800

    - add constraint to the T by using extends <T extends object | string[]>

--- learn-typescript/generics.ts
+++ learn-typescript/generics.ts
@@ -20,3 +20,13 @@ console.log(addId(user));
 // alternatively
 const result = addId(user);
 const result2 = addId<IUser>(user);
+
+// adding constraint to the conts
+const addId2 = <T extends object | string[]>(obj: T) => {
+  const id = Math.random().toString(16);
+  // mesh the id and obj together by spreading the obj
+  return {
+    id,
+    ...obj,
+  };
+};
