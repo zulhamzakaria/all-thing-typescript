4c2b6a7b5f8b7f692846b1611616a1d0311901f8
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-19 14:01:24 +0800

    - type

---
+++ learn-typescript/type.ts
@@ -0,0 +1,5 @@
+// this set hello as type 'world' as ure not allowed to change const
+const hello = "world";
+
+// this set the hello2 to type string since let allows for mutation
+let hello2 = "world";
