3c7ac14678bd5541d9fbcaf949fe205064aacbff
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-23 23:13:01 +0800

    - predicate

--- advanced-typescript-2/array-filter.ts
+++ advanced-typescript-2/array-filter.ts
@@ -3,3 +3,8 @@ const array = ["hello", null, "world"];
 const upperCaseStrnigs = array
   .filter((item): item is string => typeof item === "string")
   .map((item) => item.toUpperCase());
+
+  // updated version of TS doesnt require u to use predicate
+const upperCaseStrings = array
+  .filter((item) => typeof item === "string")
+  .map((item) => item.toUpperCase());
