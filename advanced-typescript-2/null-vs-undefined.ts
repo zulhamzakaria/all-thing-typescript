c9774511b7848c00d47048322765d9414fe01165
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-23 21:35:40 +0800

    - null, undefined

---
+++ advanced-typescript-2/null-vs-undefined.ts
@@ -0,0 +1,6 @@
+// undefined = uninitialized
+// null = inability to present a value?
+// use == to get null, undefined
+// use != null to filter out null, undefined
+let example;
+console.log(example);
