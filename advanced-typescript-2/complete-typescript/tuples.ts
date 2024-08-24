ae2d69b804f162de9236c11d12f4e1c5b320b12c
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-24 22:03:16 +0800

    - tuple is an array with explicit/defined types and length

---
+++ advanced-typescript-2/complete-typescript/tuples.ts
@@ -0,0 +1,6 @@
+let student = ["jonny", "Bsc Math"]; // this is an array
+
+// tuple set a definite length with definite types
+let student2: [string, string, number] = ["krampus", "Dark Arts", 900];
+
+let student3: [string, string][];
