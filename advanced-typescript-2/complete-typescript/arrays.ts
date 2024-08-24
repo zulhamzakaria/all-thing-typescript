b8454ed671c915495fcca4335bbb0e2321ee620c
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-24 21:54:43 +0800

    - noUncheckedIndexedAccess:true setting to avoid out of bound error

--- advanced-typescript-2/complete-typescript/arrays.ts
+++ advanced-typescript-2/complete-typescript/arrays.ts
@@ -10,3 +10,9 @@ let someVar: Array<string> | number;
 someVar = 12;
 someVar = ["1", "2", "3", "4", "5"];
 console.log(someVar.length); // based on the latest defined value
+// add noUncheckedIndexedAccess: true setting to avoid out-of-bound error
+console.log(someVar[3]);
+
+for (let num of someVar) {
+  console.log(num);
+}
