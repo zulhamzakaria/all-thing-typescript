fcb851f2c4910d8ace6b8af09ca7f5fd081d1f3d
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-24 20:24:22 +0800

    - core datatypes - to execute a  TS file: 1. tsc file.ts 2. node file.js

---
+++ advanced-typescript-2/complete-typescript/core-datatypes.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// string, number, boolean
+var name = "jonny";
+// `` allows the string to be split bu it creates a new line i/e cock\nburn
+var someVar = "cock\nburn";
+console.log(someVar);
+
+let num = 100;
---
+++ advanced-typescript-2/complete-typescript/core-datatypes.ts
@@ -0,0 +1,16 @@
+export {};
+// string, number, boolean
+const name: string = "jonny";
+// `` allows the string to be split bu it creates a new line i/e cock\nburn
+const someVar = `cock
+burn`;
+console.log(someVar);
+
+let num1 = 100;
+let num2 = 1.0;
+let num3 = 0.1;
+let num4 = 1_000_000;
+
+// boolean also works on truthy/falsey i.e 0, 100, '', ' '
+let yes = true;
+let no = false;
