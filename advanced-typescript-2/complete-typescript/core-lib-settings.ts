daeac37bd2ffe9b8752cdd2a5ca524b86a747967
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-26 23:45:30 +0800

    - libs to be included based on targeted TS version

---
+++ advanced-typescript-2/complete-typescript/core-lib-settings.ts
@@ -0,0 +1,8 @@
+// run tsc --init to convert a project to TS project
+
+/*
+1. related to 'target' setting
+2. libs included is based on the target version
+3. can also be specified like so:
+    "lib": ["es2016", "dom"]
+*/
