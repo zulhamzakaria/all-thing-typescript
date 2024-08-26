1c222c8eaed34f4a15d522bce7252f784c361605
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-26 23:38:12 +0800

    - some version may only be supported by modern browsers

---
+++ advanced-typescript-2/complete-typescript/compilation-target.ts
@@ -0,0 +1,8 @@
+/* tsc --init to convert a project to a TS project */
+
+/*
+- for targeting which version of TS for compilation
+- may pick which version thats suitable to the browsers that are being used
+
+
+*/
