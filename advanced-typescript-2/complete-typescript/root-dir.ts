e9680f7815ad2097cc2b9a70d333ed69dfc931a0
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-27 00:06:13 +0800

    - setting to define the proj root folder

---
+++ advanced-typescript-2/complete-typescript/root-dir.ts
@@ -0,0 +1,10 @@
+// run tsc --init to turn a project to a TS project
+
+/*
+1. setting to define the proj root folder for compilation
+2. "rootDir": "./src"
+3. works together with "include" settings by telling TS to only compile files inside the defined rootDir
+4. "include": ["src"]
+5. use "outDir" for generated JS files like so : "outDir":"./dist"
+6. generated/output files follow the same file structure
+*/
