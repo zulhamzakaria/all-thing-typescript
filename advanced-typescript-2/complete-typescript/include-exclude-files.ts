e08ef06318bce56cf0eaa3806a59799cff02fa62
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-26 23:30:48 +0800

    - exclude, include also work on folders

---
+++ advanced-typescript-2/complete-typescript/include-exclude-files.ts
@@ -0,0 +1,10 @@
+/*
+tsc --init to conver proj to a TS project
+that will create tsconfig.json
+
+inside tsconfig:
+----------------
+1. "exclude":['config.ts',"*.dev.ts", 'src/texts.ts']
+2. "inlcude":['config.ts',"*.dev.ts"]
+
+*/
