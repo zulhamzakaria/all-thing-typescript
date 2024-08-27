0466bb171cf0ca578375e6301d3f83505dc18da7
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-27 14:31:14 +0800

    - code quality settings

---
+++ advanced-typescript-2/complete-typescript/code-quality-settings.ts
@@ -0,0 +1,7 @@
+// run tsc --init to turn a project to the TS project
+
+/*
+1. 'noUnusedLocals' : prevent creation of unused vars. proj wont compile if theres any
+2. 'noUnusedParameters' : for unused function arguments
+3. 'noImplicitReturns' : checks if function doesnt return value on some path
+*/
