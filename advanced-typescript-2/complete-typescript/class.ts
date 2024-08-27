c0005af95f56941caeef1727b2c4525ca7504503
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-27 18:25:58 +0800

    - access modifier

--- advanced-typescript-2/complete-typescript/class.ts
+++ advanced-typescript-2/complete-typescript/class.ts
@@ -1,13 +1,21 @@
 class User {
+  private lastName: string;
+  phoneNumber: string;
   constructor(
+    phoneNumber: string,
+    lastName: string,
     public name?: string,
     public age?: number,
     public gender?: string
-  ) {}
+  ) {
+    this.lastName = lastName;
+    this.phoneNumber = phoneNumber;
+  }
 
   displayDetails() {
     console.log(`a ${this.gender} student named ${this.name}, age ${this.age}`);
   }
 }
 
-const user1 = new User();
+const user1 = new User("0198908900", "smith");
+user1.phoneNumber = "0177899090";
