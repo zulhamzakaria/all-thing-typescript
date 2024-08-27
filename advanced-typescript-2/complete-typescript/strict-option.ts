c1c083aaf18a6350241a2597642dcd48a0ab4c88
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-27 14:17:37 +0800

    - by default 'strict' setting covers all type checking settings

---
+++ advanced-typescript-2/complete-typescript/strict-option.ts
@@ -0,0 +1,7 @@
+// run tsc --init to convert a project to the TS project
+
+/*
+1. 'noImplicitAny' : no default any type for variables
+2. 'strictNullChecks' : throw error for potential null value
+3. ???
+*/
