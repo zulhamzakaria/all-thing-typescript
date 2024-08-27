e6e36391a8c914c87ecc60411ee326e606fbff3b
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-27 15:15:25 +0800

    - pack values into an array - also denoted  by ... - works on LHS of the syntax

---
+++ advanced-typescript-2/complete-typescript/rest-pattern.ts
@@ -0,0 +1,16 @@
+// assign the remaining elements to ...rest
+let [a, b, ...rest] = [1, 2, 3, 4, 5];
+
+function addNumbers(numbers: number[]) {
+  //do somn
+}
+
+addNumbers([1]);
+addNumbers([1, 2]);
+addNumbers([1, 2, 3]);
+
+function addNumbers2(...numbers: number[]) {
+  //do somn
+}
+
+addNumbers2(1, 2);
