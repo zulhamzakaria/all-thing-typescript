eef58c17ee5ddc7e022ece4bce3b7e17d1b244f3
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-27 18:14:57 +0800

    - basic method

--- advanced-typescript-2/complete-typescript/class.ts
+++ advanced-typescript-2/complete-typescript/class.ts
@@ -4,6 +4,10 @@ class User {
     public age?: number,
     public gender?: string
   ) {}
+
+  displayDetails() {
+    console.log(`a ${this.gender} student named ${this.name}, age ${this.age}`);
+  }
 }
 
 const user1 = new User();
