b2e4d2ff922157ae9214493f47682e370bcc8bbd
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-27 14:06:36 +0800

    - setting for not generating js files - usable if the project is big

---
+++ advanced-typescript-2/complete-typescript/no-emit.ts
@@ -0,0 +1,8 @@
+// run tsc --init to convert a project to the TS project
+
+/*
+1. "noEmit" setting to stop the compiler from generating .js files
+2. for checking the TS file doesnt produce any compiler error
+3. alternatively, use 'noEmitOnError'
+4. 'removeComments' to stop compiler from generating comments from the TS file
+*/
