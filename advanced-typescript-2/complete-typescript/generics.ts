e9a77c71f33bd5b9bd88ab7c3dbe2d950978daa7
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-28 16:10:38 +0800

    - multiple generics

--- advanced-typescript-2/complete-typescript/generics.ts
+++ advanced-typescript-2/complete-typescript/generics.ts
@@ -7,3 +7,7 @@ function someFunction<T>(value: T) {
 
 console.log(someFunction<string>("123"));
 console.log(someFunction<number>(123));
+
+function someGenericFunction<T, U>(obj1: T, obj2: U) {
+  //do somn
+}
