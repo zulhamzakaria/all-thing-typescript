6ee1b23678362de4f4e6b8ed18664b8562953d1c
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-28 16:14:53 +0800

    - extends usage with generics - prohibit generics to accept anything other than extended type - needed if specific methods are required

--- advanced-typescript-2/complete-typescript/generics.ts
+++ advanced-typescript-2/complete-typescript/generics.ts
@@ -8,6 +8,6 @@ function someFunction<T>(value: T) {
 console.log(someFunction<string>("123"));
 console.log(someFunction<number>(123));
 
-function someGenericFunction<T, U>(obj1: T, obj2: U) {
+function someGenericFunction<T extends object, U>(obj1: T, obj2: U) {
   //do somn
 }
