81a697ba0e04d329deaa4eee55b56b201e323dbb
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-29 20:21:14 +0800

    - involves tsconfig and webpack config

--- advanced-typescript-2/complete-typescript/webpack-typesctipr-support.ts
+++ advanced-typescript-2/complete-typescript/webpack-typesctipr-support.ts
@@ -1,19 +1,28 @@
 /*
 - setting: "sourceMap": true
-- package.json, inside 'scripts', under "test": add "build": "webpack"
+- package.json, inside 'scripts', under "test": add "build": "webpack". under 'build', add 'start':'webpack serve'
+- run npm start instead of npm run dev
+- webpack needs file inside public folder. modify the webpack config
 - content:
     const path = require('path')
     module.exports = {
+        mode: 'development'
         entry:'./array.js',
         output:{
-            filename: 'bundle.js'
-            path: path.require(__dirname, 'dist')
+            filename: 'bundle.js',
+            path: path.require(__dirname, 'dist'),
+            publicPath: '/dist/'
         },
         devtool: 'inline-source-map',
         module: {
             rules: [
                 {test: /\.ts$/, use: 'ts-loader', excludes: /node_modules/}
             ]
+        },
+        devServer:{
+            static:{
+               directory: path.resolve(__dirname, './')
+            }
         },
         resolve: {extensions:['.ts', '.js']}
     }
