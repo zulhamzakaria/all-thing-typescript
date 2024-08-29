7f6671d245d2e99af739e2b1018eff8be8066071
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-29 16:10:34 +0800

    - import/export syntax

--- advanced-typescript-2/complete-typescript/es6-modules.ts
+++ advanced-typescript-2/complete-typescript/es6-modules.ts
@@ -1,7 +1,11 @@
 /*
 - dont use namespace
 - use import keyword
-
+- use * as actions to import all. (alias is required)
+- actions.CreateUser()
+- export default class User
+- named export requires {}
+- for default export, directly import the file i.e import User from './User.js'
 */
 
 export class User {
