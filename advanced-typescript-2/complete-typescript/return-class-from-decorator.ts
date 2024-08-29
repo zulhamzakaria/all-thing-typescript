2587d446222815f61555ee82966165d6e47dc380
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-29 13:37:06 +0800

    - in this case, decorator definition of the new class will supersede the base class

--- advanced-typescript-2/complete-typescript/return-class-from-decorator.ts
+++ advanced-typescript-2/complete-typescript/return-class-from-decorator.ts
@@ -1,6 +1,21 @@
+@Logger
 class Student {
   name: string;
-  constructor(n:string){
-    this.name = n
+  constructor(n: string) {
+    this.name = n;
   }
 }
+
+function Logger(target: new (...args: any) => {}): any {
+  class LoggingClass extends target {
+    static institution: string = "UMT";
+    constructor(...args: any[]) {
+      // calls the parent class like u'd do with base/sub class
+      super(...args);
+      console.log("new logging class");
+    }
+  }
+  return LoggingClass;
+}
+
+const student01 = new Student("jonni");
