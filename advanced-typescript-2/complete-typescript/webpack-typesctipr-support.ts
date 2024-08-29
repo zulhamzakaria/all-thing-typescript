db965c65b87c0529607b95ac956950ccb033af1d
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-29 21:12:54 +0800

    - webpack prod server setting (WIP)

--- advanced-typescript-2/complete-typescript/webpack-typesctipr-support.ts
+++ advanced-typescript-2/complete-typescript/webpack-typesctipr-support.ts
@@ -3,7 +3,9 @@
 - package.json, inside 'scripts', under "test": add "build": "webpack". under 'build', add 'start':'webpack serve'
 - run npm start instead of npm run dev
 - webpack needs file inside public folder. modify the webpack config
-- content:
+- in development mode, file is generated inside memory
+- for production mode, create a new config file. name can be anything 
+- content (dev):
     const path = require('path')
     module.exports = {
         mode: 'development'
@@ -26,5 +28,29 @@
         },
         resolve: {extensions:['.ts', '.js']}
     }
-
+- content (prod):
+    const path = require('path')
+    module.exports = {
+        mode: 'production'
+        entry:'./array.js',
+        output:{
+            filename: 'bundle.js',
+            path: path.require(__dirname, 'dist'),
+        },
+        devtool: 'none',
+        module: {
+            rules: [
+                {test: /\.ts$/, use: 'ts-loader', excludes: /node_modules/}
+            ]
+        },
+        devServer:{
+            static:{
+               directory: path.resolve(__dirname, './')
+            }
+        },
+        resolve: {extensions:['.ts', '.js']},
+        plugin: [
+            
+        ]
+    }
 */
