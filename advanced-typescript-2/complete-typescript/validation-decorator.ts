2ca54aee70976fb5c2aca600d49df4ca5bd11da9
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-29 14:48:59 +0800

    - this is why u use third party libs

--- advanced-typescript-2/complete-typescript/validation-decorator.ts
+++ advanced-typescript-2/complete-typescript/validation-decorator.ts
@@ -15,14 +15,42 @@ class Inventory {
 const prod1 = new Inventory("android tablet", "AT-0009");
 
 function required(target: any, propName: string) {
+  let previousElement: string[] = [];
+  if (validateObject[target.constructor.name]) {
+    // if it;s empty, assign a value. else u'd get undefined error
+    previousElement = validateObject[target.constructor.name][propName] ?? [];
+  }
+
   // get class name
   validateObject[target.constructor.name] = {
-    [propName]: ["required"],
+    // spread operator so that the the validators are piled-up
+    // get the object only if it exists
+    ...validateObject[target.constructor.name],
+    [propName]: [...previousElement, "required"],
   };
 }
 
 function validate(obj: object): boolean {
-  return true;
+  const validateClass = validateObject[obj.constructor.name];
+  // no validation decorator in-use, so it's undefined
+  if (!validateClass) {
+    return true;
+  }
+
+  let isValid = true;
+
+  for (let prop in validateClass) {
+    for (let elem of validateClass[prop]) {
+      switch (elem) {
+        case "required":
+          //@ts-ignore
+          isValid = isValid && !!obj[prop];
+          break;
+      }
+    }
+  }
+
+  return isValid;
 }
 
 if (!validate(prod1)) {
