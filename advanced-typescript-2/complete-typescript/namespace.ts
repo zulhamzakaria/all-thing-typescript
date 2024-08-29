f44128e584874fdc12c3f0c4faa95ccbe8f439a9
zulhamzakaria <m.zulham.zakaria@gmail.com>
2024-08-29 15:20:13 +0800

    ///<reference path='./user-action.ts'> namespace UserAction{ createUser('jonni') }
    - to generate only single JS file, set the output file for the outFile setting i.e 'outFile':'./dist/bundle.js' then set the 'module" to amd

--- advanced-typescript-2/complete-typescript/namespace.ts
+++ advanced-typescript-2/complete-typescript/namespace.ts
@@ -1,5 +1,20 @@
 /*
 - code organizer for modular project
 - all files that share the same namespace will be compiled/bundled into one JS file
+- logical groupings
 */
 
+namespace UserAction {
+  let userList: User[] = [];
+  class User {
+    constructor(public name: string) {}
+  }
+
+  export function createUser(name: string) {
+    const user = new User(name);
+    userList.push(user);
+  }
+  export function getUsers() {
+    return userList;
+  }
+}
