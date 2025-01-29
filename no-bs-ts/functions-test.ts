a5d107107486494a00da54db273f87d43f646102
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-01-29 21:16:13 +0800

    - functions

---
+++ no-bs-ts/functions-test.ts
@@ -0,0 +1,3 @@
+import { addNumbers, addNumbers2 } from "./functions";
+
+console.log(addNumbers2(1, 2));
---
+++ no-bs-ts/functions.ts
@@ -0,0 +1,7 @@
+export function addNumbers(a: number, b: number): number {
+  return a + b;
+}
+
+export const addNumbers2 = (a: number, b: number) => {
+  return a + b;
+};
