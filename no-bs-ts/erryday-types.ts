3d70333589694a9db8272985d5c4cc7cd18053a4
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-01-29 20:58:34 +0800

    - Record<T,U> is the dictionary

--- no-bs-ts/erryday-types.ts
+++ no-bs-ts/erryday-types.ts
@@ -28,3 +28,18 @@ const student01: student = {
   age: 19,
   addressLines: ["jln abc", "bandar cde"],
 };
+
+const name2 = student01.name;
+
+let newVar: Record<string, string>;
+
+const ids: Record<string, number> = {
+  john: 20,
+  kid: 22,
+};
+ids["tom"] = 16;
+
+// satisfies only work on type?
+if (ids["ellis"] === 22) {
+  [1, 2, 3, 4].forEach((v) => console.log(v));
+}
