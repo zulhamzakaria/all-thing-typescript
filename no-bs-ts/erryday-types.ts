3d5f1a740ec63115e620ea4a86f651d4ef29138a
zulhamzakaria <m.zulham.zakaria@gmail.com>
2025-01-29 20:41:34 +0800

    - wip

--- no-bs-ts/erryday-types.ts
+++ no-bs-ts/erryday-types.ts
@@ -6,3 +6,25 @@ let bool: boolean = true;
 let regex: RegExp = /foo/;
 
 const names: string[] = username.split(",");
+
+const Person = {
+  name: "jack",
+  age: 18,
+};
+
+type people = {
+  name: string;
+  age: number;
+};
+
+type address = {
+  addressLines: string[];
+};
+
+type student = people & address;
+
+const student01: student = {
+  name: "john",
+  age: 19,
+  addressLines: ["jln abc", "bandar cde"],
+};
